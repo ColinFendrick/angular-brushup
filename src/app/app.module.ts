2cd20bc1c7ac8f181bcd07b97f9cf2568aa8a810
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-03 13:38:23 -0800

    Update routing and dom, lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +24,13 @@ import { AboutComponent } from './about/about.component';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		HttpClientModule
+		HttpClientModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		MatCardModule,
+		MatProgressSpinnerModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
